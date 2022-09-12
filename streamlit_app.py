ba6deb87ccac6a48d580add5b9b427b68c1fe41b
Devika Kota <113369488+devika1030@users.noreply.github.com>
2022-09-12 12:23:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Roket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Roket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
 
 
 
