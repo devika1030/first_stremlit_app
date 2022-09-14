7567609b04a355a534552a125313dda161e2609e
Devika Kota <113369488+devika1030@users.noreply.github.com>
2022-09-14 09:43:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,8 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 # Let's put a pick list here so they can pick the fruit they want to include 
 add_my_fruit = streamlit.multiselect("What fruit would you like to have?:", list(my_fruit_list.index))
 
+streamlit.write('Thanks for adding ',add_my_fruit)
+
 #my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
