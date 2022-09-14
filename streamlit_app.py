a076b466bb81d418c980ab5202a4664820413cd5
Devika Kota <113369488+devika1030@users.noreply.github.com>
2022-09-14 10:21:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 
 streamlit.write('Thanks for adding ', fruit_choice)
 
-#my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
 
